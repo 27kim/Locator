a47e3d32cb28ccb63acbec0bf7ffe46b27bd8d37
27 Kim <doohyeonkim@gmail.com>
2019-09-22 13:04:51 +0900

    googleApi연결

--- app/src/main/java/com/d27/locator/LocatrActivity.java
+++ app/src/main/java/com/d27/locator/LocatrActivity.java
@@ -4,13 +4,34 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+
 public class LocatrActivity extends SingleFragmentActivity {
+    private static final int REQUEST_ERROR = 0;
 
     @Override
     Fragment createFragment() {
         return LocatrFragment.newInstance();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        int errorCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (errorCode != ConnectionResult.SUCCESS) {
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(errorCode, this, REQUEST_ERROR,
+                    new DialogInterface.OnCancelListener() {
+                        @Override
+                        public void onCancel(DialogInterface dialogInterface) {
+                            finish();
+                        }
+                    });
+        }
     }
 }
--- app/src/main/java/com/d27/locator/LocatrFragment.java
+++ app/src/main/java/com/d27/locator/LocatrFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,8 +13,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 public class LocatrFragment extends Fragment {
     private ImageView mImageView;
+    private GoogleApiClient mApiClient;
 
     public static LocatrFragment newInstance() {
 
@@ -22,6 +27,29 @@ public class LocatrFragment extends Fragment {
         LocatrFragment fragment = new LocatrFragment();
         fragment.setArguments(args);
         return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+
+        mApiClient = new GoogleApiClient.Builder(getActivity())
+                .addApi(LocationServices.API)
+                //연결상태 정보 확인 위한 callback 추가
+                .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+                    @Override
+                    public void onConnected(@Nullable Bundle bundle) {
+                        
+                        getActivity().invalidateOptionsMenu(); //calls onPrepareOptionsMenu()
+                    }
+
+                    @Override
+                    public void onConnectionSuspended(int i) {
+
+                    }
+                })
+                .build();
     }
 
     @Nullable
@@ -33,14 +61,24 @@ public class LocatrFragment extends Fragment {
     }
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
+    public void onStart() {
+        super.onStart();
+        getActivity().invalidateOptionsMenu(); //calls onPrepareOptionsMenu()
+        mApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        mApiClient.disconnect();
+        super.onStop();
     }
 
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         inflater.inflate(R.menu.menu_locatr, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_locate);
+        searchItem.setEnabled(mApiClient.isConnected());
     }
 }
